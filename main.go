dd6362a35372ce2fa3f5453e848a6eb86c7a35b7
Pacoxdliu <107404543+Pacoxdliu@users.noreply.github.com>
2024-06-23 18:08:19 +0800

    add parse FS

--- main.go
+++ main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/PacoXXD/lenslock/controller"
+	"github.com/PacoXXD/lenslock/templates"
 	"github.com/PacoXXD/lenslock/views"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
-func execTemplate(w http.ResponseWriter, filepath string) {
-	t, err := views.Parse(filepath)
-	if err != nil {
-		fmt.Printf("Parse html failed %s", err)
-		http.Error(w, "There is an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
+// func execTemplate(w http.ResponseWriter, filepath string) {
+// 	t, err := views.Parse(filepath)
+// 	if err != nil {
+// 		fmt.Printf("Parse html failed %s", err)
+// 		http.Error(w, "There is an error parsing the template.", http.StatusInternalServerError)
+// 		return
+// 	}
 
-	t.Execute(w, nil)
-	// data, err := os.ReadFile(filepath)
-	// if err != nil {
-	// 	fmt.Printf("Read html failed %s", err)
-	// 	http.Error(w, "There is an error reading the template.", http.StatusInternalServerError)
-	// 	return
-	// }
-	// w.Write(data)
-}
+// 	// t.Execute(w, nil)
+// 	// data, err := os.ReadFile(filepath)
+// 	// if err != nil {
+// 	// 	fmt.Printf("Read html failed %s", err)
+// 	// 	http.Error(w, "There is an error reading the template.", http.StatusInternalServerError)
+// 	// 	return
+// 	// }
+// 	// w.Write(data)
+// }
 
 // func homeHandler(w http.ResponseWriter, r *http.Request) {
 // 	tmpPath := filepath.Join("templates", "home.gohtml") //lenslocked/templates/contact.gohtml
@@ -89,25 +89,16 @@ func main() {
 
 	// var router Router
 	r := chi.NewRouter()
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/", controller.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/contact", controller.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "support.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/faq", controller.StaticHandler(tpl))
-
 	r.Use(middleware.Logger)
+
+	r.Get("/", controller.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gotmp"))))
+	r.Get("/contact", controller.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gotmp"))))
+	r.Get("/faq", controller.StaticHandler(views.Must(views.ParseFS(templates.FS, "support.gotmp"))))
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not Found", http.StatusNotFound)
+
+	})
 
 	// err := http.ListenAndServe(":8080", mux)
 	fmt.Println("Server is listening on :8080")
---
+++ templates/fs.go
@@ -0,0 +1,8 @@
+package templates
+
+import "embed"
+
+// FS contains the templates
+//
+//go:embed *
+var FS embed.FS
